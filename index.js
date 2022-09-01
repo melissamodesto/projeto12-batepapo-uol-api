ed69e06ebb60ff30255462fc93bc3594f4c442df
Melissa Modesto <mbm.modesto@gmail.com>
2022-09-01 15:40:06 -0300

    Add Endpoint's /participants

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-import express from 'express';
-import cors from 'cors';
+import express, { json } from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import chalk from "chalk";
+dotenv.config();
 
-const server = express()
+const server = express();
+server.use(cors());
+server.use(json());
 
-server.use(cors())
-server.use(express.json())
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+let db;
 
+mongoClient.connect().then(() => {
+  db = mongoClient.db("batepapoUol");
+});
 
-server.listen(5000, () => console.log('listening on port 5000'))
+server.get("/participants", (req, res) => {
+  db.collection("participants")
+    .find()
+    .toArray()
+    .then((data) => {
+      res.status(200).send(data);
+    });
+});
+
+server.post("/participants", (req, res) => {
+  const { name } = req.body;
+
+  if (!name || name.length === 0) {
+    res.status(400).send("Nome de usuário é obrigatório");
+  } else {
+
+      db.collection("participants").insertOne({ name });
+      res.status(201).send("Login realizado com sucesso");
+  }
+
+});
+
+server.listen(5000, () => console.log(chalk.yellow("listening on port 5000")));
